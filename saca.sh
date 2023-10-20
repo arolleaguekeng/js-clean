8914b4aacf0b97345c83225e764f38ef11d94073
arolleaguekeng <arolleaguekeng@gmail.com>
2023-10-20 17:35:18 +0100

    transform to functions, add gitignore

--- saca.sh
+++ saca.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo
 echo "================================================================"
 echo "************ Shell Angular Clean Architecture ******************"
@@ -6,50 +7,62 @@ echo "-------------- Copyright Arolle Aguekeng -----------------------"
 echo "================================================================"
 echo
 
-ng new devpea-site --routing --style=scss --skip-tests=true --skip-install=true
-cd devpea-site/src/app
+createAngularProject(){
+    ng new devpea-site --routing --style=scss --skip-tests=true --skip-install=true
+    cd devpea-site/src/app
+}
 
-mkdir core
-mkdir domain
-mkdir data
-mkdir presentation
+createArchitectureFolders() {
+    mkdir core
+    mkdir domain
+    mkdir data
+    mkdir presentation
+}
 
-# Manage Core Configuration
-touch core/mapper.ts
-touch core/readme.md
+configureDomain(){
+    # Manage Domain Configuration
+    mkdir domain/base
+    touch domain/base/readme.md
+    touch domain/base/user-case.ts
 
-# Manage Presentation Configuration
-mkdir presentation/components
-touch presentation/readme.md
-touch presentation/components/readme.md
+    mkdir domain/models
+    touch domain/readme.md
+    touch domain/models/readme.md
+    touch domain/models/user.model.ts
 
-# Manage Domain Configuration
-mkdir domain/base
-touch domain/base/readme.md
-touch domain/base/user-case.ts
+    mkdir domain/repositories
+    touch domain/repositories/readme.md
+    touch domain/repositories/user.repository.ts
 
-mkdir domain/models
-touch domain/readme.md
-touch domain/models/readme.md
-touch domain/models/user.model.ts
+    mkdir domain/usecases
+    touch domain/readme.md
+    touch domain/usecases/readme.md
+    touch domain/usecases/user.usecase.ts
+    touch domain/usecases/user-register.usecase.ts
+    # End Of domain configuration
+}
 
-mkdir domain/repositories
-touch domain/repositories/readme.md
-touch domain/repositories/user.repository.ts
+configureCore(){
+    # Manage Core Configuration
+    touch core/mapper.ts
+    touch core/readme.md
+}
 
-mkdir domain/usecases
-touch domain/readme.md
-touch domain/usecases/readme.md
-touch domain/usecases/user.usecase.ts
-touch user-register.usecase.ts
-# End Of domain configuration
+configurePresentation(){
+    # Manage Presentation Configuration
+    mkdir presentation/components
+    touch presentation/readme.md
+    touch presentation/components/readme.md
+}
 
-# Manage Data Configuration
-mkdir -p data/repositories/user/entities
-touch data/repositories/user/entities/user.entity.ts
-touch readme.md
-mkdir -p data/repositories/user/mappers
-touch data/repositories/user/mappers/user.mapper.ts
+configureData(){
+    # Manage Data Configuration
+    mkdir -p data/repositories/user/entities
+    touch data/repositories/user/entities/user.entity.ts
+    touch readme.md
+    mkdir -p data/repositories/user/mappers
+    touch data/repositories/user/mappers/user.mapper.ts
+}
 
 
 echo "================================================================"
